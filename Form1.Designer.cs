9de2a415e8ed6784e3d468c7e4ce83d60999a2e5
Raymond Krehn <rkrehn@duck.com>
2026-01-21 09:21:39 -0700

    Updated name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -470,7 +470,7 @@
             Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             Name = "Form1";
-            Text = "YouTube Album Downloader v2.6";
+            Text = "YouTube Album Downloader v2.7";
             FormClosing += Form1_FormClosing;
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dgv_downloads).EndInit();
